ed671d2e5f9828d7914ab1c220f8f9631503352c
wangyang <1782167355@qq.com>
2026-05-14 09:56:57 +0800

    fix: 1

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
   return (
     <div className="App">
       <header className="App-header">
-        <div>name</div>
+        <div>name2</div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
