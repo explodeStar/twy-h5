76d1457f4f47c9e13ccac0d6f63840bd0b9fe10d
wangyang <1782167355@qq.com>
2026-05-14 11:23:37 +0800

    fix: fix file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>name2</div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <div>h5项目 App</div>;
 }
-
-export default App;
